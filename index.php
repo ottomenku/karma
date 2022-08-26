7b8bf7b7d76aea536393a73ea55365c71ff383c9
Ménkű Ottó <ottomenku@gmail.com>
2022-08-26 20:34:33 +0200

    fvsdfv

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
-nophp
-
 <?php
+use eftec\bladeone\BladeOne;
 
-echo 'phphphphp';
+ $views = __DIR__ . '/views';
+  $cache = __DIR__ . '/cache';
+ $blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
+ echo $blade->run("base2",['spiriszam'=>8]); // it calls /views/hello.blade.php
+
 ?>
---
+++ views/base2.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="HU">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Karma 1.0</title>
+     
+    </head>
+    <body>
+<h4> Karma 1.0</h4>
+<h3> Spiriszam: {{ $spiriszam] }}</h3>
+
+
+    </body>
+</html>
