29324dad71c6a090cc99504e408bb79e2aef94be
Ménkű Ottó <ottomenku@gmail.com>
2022-08-26 21:09:23 +0200

    cj,j,

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 use Jenssegers\Blade\Blade;
-$blade = new Blade('views', 'cache');
+//$blade = new Blade('views', 'cache');
 echo 'sgdhsdfghsdfgh';
 //echo $blade->make('base2', ['name' => 'John Doe'])->render();
 
