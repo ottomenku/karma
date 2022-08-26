21094957bae3f6daecbeb85283d97fcfdf2a281c
Ménkű Ottó <ottomenku@gmail.com>
2022-08-26 20:37:42 +0200

    qwedwefd

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 use eftec\bladeone\BladeOne;
 
- $views = __DIR__ . '/views';
-  $cache = __DIR__ . '/cache';
- $blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
- echo $blade->run("base2",['spiriszam'=>8]); // it calls /views/hello.blade.php
+$views = __DIR__ . '/views';
+$cache = __DIR__ . '/cache';
+$blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG); // MODE_DEBUG allows to pinpoint troubles.
+echo $blade->run("base2",array("spiriszam"=>"value1")); // it calls /views/hello.blade.php
 
 ?>
